22b118567892727fdf1faad11ffdcfd10a8d6a6d
mooden <alexandrostruses@yandex.ru>
2016-02-03 08:22:00 +0400

    docs

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	$('h1,h2').each(function(i,d){
+		var section = $(d).attr('id')
+
+		$.get(
+			'pages/' + section + '.html',
+			function(data){
+				$(d)
+					.after(data)
+					.next()
+					.find('code[src]')
+					.each(function(i,c){
+						var src = $(c).attr('src')
+
+						$.get(
+							'code/' + src + '.src',
+							function(data){
+								$(c)
+									.text(data)
+									.removeAttr('src')
+
+								Prism.highlightElement($(c).get(0))
+							}
+						)
+					})
+
+			}
+		)
+	})
+})
